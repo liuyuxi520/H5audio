4e10ac9432c8eada12c1f23f0a1a14dc372356d4
刘雨熙 <liuyuxi@liuyuxideMBP.lan>
2018-01-16 23:37:48 +0800

    修改方法，返回对象方便下次调用方法

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-(function(){
+window.musicFn =(function(){
     function audios(name) {//播放暂停音频
         var myAudio =document.getElementById(name);
         if(myAudio.paused){
@@ -10,22 +10,40 @@
     function toggleName(node,classname){
         node.toggleClass(classname);
     }
-    $('.js-audioImg').on('click',function(){
-        var $this = $(this);
-        toggleName($this,'circle');
-        audios('js-music');
-    });
-
-    function beginMusic(node,musicBtn){
+    function toggleImg(node,url1,url2){
+        var url = node.attr('src');
+        if(url.indexOf(url1) > -1 ){
+            node.attr('src',url2);
+        }else{
+            node.attr('src',url1);
+        }
+    }
+    function beginMusic(node,musicBtn,musicBgUrl){
         var shapeAudio=document.getElementById(node),
             btn = document.getElementsByClassName(musicBtn);
-        btn[0].classList.add('circle');  
+        btn[0].classList.add('circle');
+        btn[0].src = musicBgUrl;  
         shapeAudio.pause();
         shapeAudio.play();
     }
     
-    $('.js-begin').on('click',function(){
-        beginMusic('js-music','js-audioImg');
-    });      
+    function start(){
+        $('.js-audioImg').on('click',function(e){//右上角音乐小图片
+            var $this = $(this),
+                target = $(e.target);
+            toggleName($this,'circle');
+            audios('js-music');
+            toggleImg(target,'/images/bg-music.png','/images/bg-music2.png');
+        });
         
-})();
+        $('.js-begin').on('click',function(){//开始按钮
+            beginMusic('js-music','js-audioImg','/images/bg-music.png');
+        });
+    }      
+    return {
+        audios : audios,
+        toggleName : toggleName,
+        beginMusic : beginMusic,
+        start : start
+    }  
+})().start();
